76e3444912343eb846976439d4d2c42ccc6da132
ahmed-farahat-pro <118939263+ahmed-farahat-pro@users.noreply.github.com>
2023-10-15 02:12:18 +0300

    Update adminstratorRoutes.js

--- server/routes/adminstratorRoutes.js
+++ server/routes/adminstratorRoutes.js
@@ -8,22 +8,13 @@ router.route("/addAdmin").post(adminController.addAdmin);
 router.route("/delAdminpharmacistPatient").delete(adminController.delAdminpharmacistPatient);
  
 
-
 router.route("/pendingDoctors").get(adminController.viewPendingpharmacists);
 
 router.get('/allmedicinesdyuse' , adminController.getMedicinesByMedicalUse)
 router.get('/available-medicines', adminController.getAvailableMedicines);
-=======
-router.route("/pendingPharmacists").get(adminController.viewPendingPharmacists);
 
-
-router.get("/available-medicines", adminController.getAvailableMedicines);
-router.get("/medicines", adminController.searchMedicine);
 // Get pharmacist details by ID
-router.get(
-  "/:pharmacistId/PharmacistDetails",
-  adminController.getPharmacistDetails
-);
+router.get('/:pharmacistId/PharmacistDetails', adminController.getPharmacistDetails);
 
 module.exports = router;
 
