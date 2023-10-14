c2fc2c4be2fe6b67f051e7be9b4ec51f9e728243
ahmed-farahat-pro <118939263+ahmed-farahat-pro@users.noreply.github.com>
2023-10-15 02:13:00 +0300

    Update patientRoutes.js

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -5,19 +5,11 @@ const patientController = require("./../controllers/patientController");
 
 
 
-
  router.route('/').get(patientController.getAllPatients);
   //  .post(patientController.createPatient);
 router.get('/filterMedicine', patientController.filterMedicine);
 router.get('/searchmedicine', patientController.searchMedicineByName);
-
-// router
-//     .route('/')
-//     .get(patientController.getAllPatients)
-//     .post(patientController.createPatient);
-router.get("/medicines", patientController.searchMedicine);
-
-mydoctors',CheckAuth, patientController.getMypharmacists);
+router.get('/mydoctors',CheckAuth, patientController.getMypharmacists);
 router.get("/:id", patientController.getPatients);
 router.patch("/add-family-members", CheckAuth ,patientController.addFamilyMembers);
 
