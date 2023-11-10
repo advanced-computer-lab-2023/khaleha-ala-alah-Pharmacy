3abf0ef69a2e0976282ee40b4b3ca092339897b2
ahmed-farahat-pro <118939263+ahmed-farahat-pro@users.noreply.github.com>
2023-11-10 03:13:40 +0200

    hi

--- server/models/order.js
+++ server/models/order.js
@@ -28,6 +28,10 @@ const orderSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  address:{
+    type: String,
+    required:true
+  }
   /*status: {
     type: String,
     enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
