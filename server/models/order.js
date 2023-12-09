df4db0b45ba2dc63f0e852a26892d55c6e5fa383
Abdelrahman-Elgoharyy <125679656+Abdelrahman-Elgoharyy@users.noreply.github.com>
2023-12-09 23:56:37 +0200

    salesReport backend

--- server/models/order.js
+++ server/models/order.js
@@ -1,46 +1,49 @@
 // models/order.js
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  userID: {
-    type:String,
-    required: true,
-  },
-  items: [
-    {
-      medicine: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Medicine",
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-      totalPrice: {
-        type: Number,
-        required: true,
-      },
+const orderSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: String,
+      required: true,
     },
-  ],
-  totalAmount: {
-    type: Number,
-    required: true,
-  },
+    items: [
+      {
+        medicine: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Medicine",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        totalPrice: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
 
-
-  address:{
-    type: String,
-    required:true
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
+      default: "Pending",
+      required: true,
+    },
   },
-  status: {
-
-    type: String,
-    enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
-    default: "Pending",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
