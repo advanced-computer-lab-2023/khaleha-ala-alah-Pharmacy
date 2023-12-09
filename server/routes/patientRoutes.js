e1a8db357100e766058bb2b145197e6f28bb6de0
Abdelrahman-Elgoharyy <125679656+Abdelrahman-Elgoharyy@users.noreply.github.com>
2023-12-09 22:09:35 +0200

    cart backend modifications

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -11,7 +11,11 @@ router.get("/filterMedicine", patientController.filterMedicine);
 router.get("/searchmedicine", patientController.searchMedicineByName);
 router.get("/myOrders/:status", CheckAuth, patientController.getMyOrders);
 router.get("/myorderDetails/", CheckAuth, patientController.getOrderDetails);
-router.patch("/cancel-order/:orderID", CheckAuth,patientController.cancelOrder);
+router.patch(
+  "/cancel-order/:orderID",
+  CheckAuth,
+  patientController.cancelOrder
+);
 router.get("/getOrderMedicine", patientController.getOrderMedicine);
 router.get("/allMediciness", patientController.getAllMedicines);
 router.get("/mydoctors", CheckAuth, patientController.getMypharmacists);
@@ -24,7 +28,11 @@ router.patch(
 
 router.post("/add-amount-Wallet", patientController.addAmountToWallet);
 router.post("/remove-from-wallet", patientController.removeAmountFromWallet);
-router.get("/amount-wallet/:userID", CheckAuth,patientController.getAmountInWallet);
+router.get(
+  "/amount-wallet/:userID",
+  CheckAuth,
+  patientController.getAmountInWallet
+);
 
 router.post("/:userId/add-address", patientController.addAddress);
 router.delete(
@@ -35,19 +43,25 @@ router.delete(
 router.get("/:userId/get-all-addresses", patientController.getAllAddresses);
 
 router.post("/save-stripe-token", paymentController.payForPackage);
-router.post("/add-to-cart", patientController.addToCart);
-router.get("/view-cart-items/:patientId", patientController.viewCartItems);
+router.post("/add-to-cart", CheckAuth, patientController.addToCart);
+router.get(
+  "/view-cart-items/:patientId",
+  CheckAuth,
+  patientController.viewCartItems
+);
 //router for the removeItemFromCart
 router.patch(
-  "/remove-from-cart/:patientId/:medicineId",
+  "/remove-from-cart/:medicineId",
+  CheckAuth,
   patientController.removeItemFromCart
 );
 // router for changeItemQuantity
 router.patch(
-  "/change-item-quantity/:patientId/:medicineId",
+  "/change-item-quantity/:medicineId",
+  CheckAuth,
   patientController.changeItemQuantity
 );
-router.post("/checkout/:patientId", patientController.checkout);
+router.post("/checkout", CheckAuth, patientController.checkout);
 //router.get('/presecriptions', CheckAuth, patientController.getPerscriptions);
 
 module.exports = router;
